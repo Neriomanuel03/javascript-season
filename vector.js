54e89544ccb8a87babbb043805133a8b15348f92
Neriomanuel03 <neriomanuel03@gmail.com>
2026-09-11 00:16:47 +0100

    comeco

---
+++ vector.js
@@ -0,0 +1,16 @@
+//criando uma lista e imprimir
+
+/* const numeros = ["1", "2", "3", "4"];
+numeros.unshift ("3"); //adiciona o ele na primeira posicao
+console.log(numeros); */
+
+
+/* const numeros = ["1", "2", "3", "4"];
+numeros.pop ("3"); //remove o ele na ultima posicao
+console.log(numeros);
+
+const numeros = ["1", "2", "3", "4"];
+numeros.unshift ("3"); //adiciona o ele na primeira posicao
+console.log(numeros);
+
+ */
